b57f4876a5f35c8548ad2ea77623aa930511f42f
stephenh0191 <stephenhughes0191@gmail.com>
2021-03-16 20:40:46 +0000

    added semantic& jquery gem file, added Semantic javascripts, added inverted menu, dropdown partial nav file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,11 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//= require semantic-ui
+$(document).on('turbolinks:load', function () {
+$('.ui.dropdown') .dropdown();
+})
