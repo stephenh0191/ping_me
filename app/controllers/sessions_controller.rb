3af87f6cc1fa8b376f969805a95ef3c7f2afd6c1
stephenh0191 <stephenhughes0191@gmail.com>
2021-03-18 21:36:45 +0000

    Update login form and add authentication method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,23 @@
 class SessionsController < ApplicationController
-	helper_method :current_user, :logged_in?
 
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
+		def new
+		end
 
-  def logged_in?
-    !!current_user
-  end
+		def create
+			user = User.find_by(username: params[:session][:username])
+			if user && user.authenticate(params[:session][:password])
+				session[:user_id] = user.id
+				flash[:success] = "You have succesfully logged in!"
+				redirect_to root_path
+			else
+				flash.now[:error] = "Username or Password was incorrect"
+				render 'new'
+			end
+		end
 
-  def require_user
-    if !logged_in?
-      flash[:alert] = "You must be logged in to perform that action"
-      redirect_to login_path
-    end
-  end
-
+		def destroy
+			session[:user_id] = nil
+			flash[:success] = "You have succesfully logged out!"
+			redirect_to login_path
+		end
 end
