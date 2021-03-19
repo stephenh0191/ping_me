114f99089f332d5cd55f9c14fff4b747c3c4d1d9
stephenh0191 <stephenhughes0191@gmail.com>
2021-03-19 23:56:52 +0000

    added footer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,7 +43,7 @@ class UsersController < ApplicationController
 
 private
   def user_params
-    params.require(:user).permit(:username, :email, :password)
+    params.permit(:username, :email, :password)
   end
 
   def set_user
