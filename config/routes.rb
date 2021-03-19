1b1414594016dfd5ed4c989950b937c4be504e3a
stephenh0191 <stephenhughes0191@gmail.com>
2021-03-19 20:27:53 +0000

    add user signup page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   post "login", to: "sessions#create"
   delete 'logout', to: "sessions#destroy"
   post 'message', to: 'messages#create'
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+
 
   mount ActionCable.server, at: '/cable'
 end
